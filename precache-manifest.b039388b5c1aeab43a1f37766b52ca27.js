90bd7a760b2080662fea9e0e4d715146b389cdae
Your Name <you@example.com>
2020-08-04 06:39:43 +0530

    Updates

---
+++ precache-manifest.b039388b5c1aeab43a1f37766b52ca27.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36406de1da2bf5ebe39fe9b72d8a17a",
+    "url": "/firstreactproject/index.html"
+  },
+  {
+    "revision": "6f3b37faf13fa13ebefb",
+    "url": "/firstreactproject/static/css/2.38451786.chunk.css"
+  },
+  {
+    "revision": "68ea4f85a3e4898924a3",
+    "url": "/firstreactproject/static/css/main.9fa706cb.chunk.css"
+  },
+  {
+    "revision": "6f3b37faf13fa13ebefb",
+    "url": "/firstreactproject/static/js/2.85900e9e.chunk.js"
+  },
+  {
+    "revision": "079f5f929dddc1c36f62c9410b1d0399",
+    "url": "/firstreactproject/static/js/2.85900e9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68ea4f85a3e4898924a3",
+    "url": "/firstreactproject/static/js/main.3a46ead0.chunk.js"
+  },
+  {
+    "revision": "844d1f2c0b095c93a9ea",
+    "url": "/firstreactproject/static/js/runtime-main.1d0b4a95.js"
+  },
+  {
+    "revision": "a29f9ecdd3ca0b0e6f4cc4447da7f260",
+    "url": "/firstreactproject/static/media/android.a29f9ecd.jpg"
+  },
+  {
+    "revision": "2ef23ef168a9f0d4982349b878b31e52",
+    "url": "/firstreactproject/static/media/com.2ef23ef1.jpg"
+  },
+  {
+    "revision": "bd414d8287d1393c18feb19de3fed0b3",
+    "url": "/firstreactproject/static/media/iot.bd414d82.jpg"
+  },
+  {
+    "revision": "8067036136ecff2e8d41d823af59585e",
+    "url": "/firstreactproject/static/media/machine.80670361.jpg"
+  },
+  {
+    "revision": "426e24f5215079ab20b8aeed47567616",
+    "url": "/firstreactproject/static/media/web.426e24f5.jpg"
+  }
+]);
